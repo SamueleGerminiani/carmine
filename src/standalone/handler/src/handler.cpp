10ecafa5e73038798c6b4cf47a2ed9589a950ab5
SamueleGerminiani <samuele.germiniani@univr.it>
2021-12-26 16:48:06 +0100

    fixed get freq - handler hotfic

--- src/standalone/handler/src/handler.cpp
+++ src/standalone/handler/src/handler.cpp
@@ -88,7 +88,6 @@ int main(int argc, char **argv) {
     fillAllTopics();
     fillAllCheckers();
 
-    initMaxFreq();
 
     // The following code is used to decide which node will contain the
     // coordinator
